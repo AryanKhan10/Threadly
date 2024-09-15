4cc5714847795f59d4a12ff41d9e930a06fb4ebb
unknown <cs.aryan.uet@gmail.com>
2024-09-15 18:56:26 +0500

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
 
   return (
-    <div className="img mt-16">
+    <div className="img min-h-screen mt-16">
       <h1 className="font-Allerta text-white text-3xl sm:text-4xl lg:text-5xl font-medium h-full flex justify-center items-center " >Login to see posts!</h1>
     </div>
   );
